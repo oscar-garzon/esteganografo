e06af50c4efc8ce1efe4f34fdacc7307c68e3fa3
Oscar Garzon <oscar.garzon@ciencias.unam.mx>
2022-04-25 18:32:35 -0500

    Se implemento ocultar y test_ocultar. Se genera documentacion para el proyecto.

---
+++ src/ocultar.py
@@ -0,0 +1,95 @@
+from random import randint
+from typing import List
+
+from PIL import Image
+import numpy as np
+
+
+def convertir_texto_a_binario(texto: str) -> List[str]:
+    """Dada una cadena de texto regresa una lista de los valores unicode
+    en binario asociado a cada caracter en la cadena.
+
+    Mayúsculas las pasa a minúsculas, remueve acentos, deja espacios en
+    blanco si encuentra un caracter inválido.
+    """
+    texto_en_binario = []
+    acentosñ_unicode = {
+        '225': 97,
+        '233': 101,
+        '237': 105,
+        '243': 111,
+        '250': 117,
+        '241': 110,
+    }
+    texto = texto.lower()
+    for caracter in texto:
+        valor_unicode_caracter = ord(caracter)
+        # remueve acentos
+        if str(valor_unicode_caracter) in acentosñ_unicode:
+            valor_unicode_caracter = acentosñ_unicode[
+                str(valor_unicode_caracter)
+            ]
+        # pone espacio en blanco en lugar de caracter inválido
+        elif valor_unicode_caracter > 127 or valor_unicode_caracter < 32:
+            valor_unicode_caracter = 32
+
+        texto_en_binario.append('{0:08b}'.format(valor_unicode_caracter))
+    return texto_en_binario
+
+
+def ocultar_car_bin_en_arr(
+    caracter: str, arr: List[List[int]], indx: int
+) -> None:
+    """Modifica arr para que oculte un caracter en tres pixeles.
+
+    El primer pixel donde se comienza a ocultar 'caracter' es arr[indx].
+    caracter debe ser la representación en binario de su valor unicode.
+    Se modifica el least significant bit de los tres pixeles(r,g,b).
+    Los colores r y g del primer pixel no se modifican.
+    """
+    for pixel_idx in range(indx, indx + 3):
+        for color_idx in range(3):
+            if pixel_idx % 3 == 0 and (color_idx == 0 or color_idx == 1):
+                continue
+            else:
+                lsb = int(caracter[3 * (pixel_idx % 3) + color_idx - 1])
+            valor = arr[pixel_idx][color_idx]
+            arr[pixel_idx][color_idx] = modifica_lsb(valor, lsb)
+
+
+def ocultar_binario_en_imagen(txt_bin: List[str], img: Image) -> np.ndarray:
+    """Oculta en el arreglo de una imagen los valores binarios de una lista.
+
+    Supone que todos los valores están entre 0-127(en binario). Añade '\z'
+    en el arreglo de la imagen para señalar el final del texto.
+    Lanza Exception cuando el txt_bin no se puede ocultar en la imagen por su
+    longitud.
+    """
+    ancho, alto = img.size
+    img_arr_lineal = np.asarray(img, np.uint8).reshape((ancho * alto, 3))
+
+    if ancho * alto < len(txt_bin)*3 + 6:
+        raise Exception('El texto es demasiado largo. No se puede ocultar en imagen')
+
+    for i, caracter in enumerate(txt_bin):
+        ocultar_car_bin_en_arr(caracter, img_arr_lineal, 3 * i)
+
+    # Añade '\z' para indicar el fin de texto.
+    for i, caracter in enumerate(['01011100', '01111010']):
+        ocultar_car_bin_en_arr(
+            caracter, img_arr_lineal, 3 * len(txt_bin) + 3 * i
+        )
+
+    return img_arr_lineal.reshape((alto, ancho, 3))
+
+
+def modifica_lsb(num_dec: int, last_bit: int) -> int:
+    """Dado un número decimal lo modifica para que su LSB sea igual a last_bit.
+
+    last_bit es 0 ó 1. Modifica el LSB de la representación binaria de num_dec.
+    Devuelve la representación decimal de num_dec modificado.
+    """
+    num_bin = '{0:08b}'.format(num_dec)
+    num_bin = num_bin[:-1] + str(last_bit)
+    num_dec = int(num_bin, 2)
+    return num_dec
---
+++ test/test_ocultar.py
@@ -0,0 +1,149 @@
+import os
+import sys
+import unittest
+
+from PIL import Image
+import numpy as np
+
+sys.path.append(os.getcwd())
+
+from src import ocultar
+
+class Tests(unittest.TestCase):
+    def test_convertir_texto_a_binario(self):
+        s = 'hola bebe!'
+        s_acentos_mayuscula = 'Hola bebé!'
+        simbolos_invalidos = 'æ es inv@lido y tæmßien'
+        simb_sencilla = 'æxßá'
+        s_unicode = [
+            '01101000',
+            '01101111',
+            '01101100',
+            '01100001',
+            '00100000',
+            '01100010',
+            '01100101',
+            '01100010',
+            '01100101',
+            '00100001',
+        ]
+        simbolos_invalidos_unicode = [
+            '00100000',
+            '00100000',
+            '01100101',
+            '01110011',
+            '00100000',
+            '01101001',
+            '01101110',
+            '01110110',
+            '01000000',
+            '01101100',
+            '01101001',
+            '01100100',
+            '01101111',
+            '00100000',
+            '01111001',
+            '00100000',
+            '01110100',
+            '00100000',
+            '01101101',
+            '00100000',
+            '01101001',
+            '01100101',
+            '01101110',
+        ]
+        simb_sencilla_unicode = [
+            '00100000',
+            '01111000',
+            '00100000',
+            '01100001',
+        ]
+        self.assertListEqual(ocultar.convertir_texto_a_binario(s), s_unicode)
+        self.assertListEqual(
+            ocultar.convertir_texto_a_binario(s_acentos_mayuscula), s_unicode
+        )
+        self.assertListEqual(
+            ocultar.convertir_texto_a_binario(simbolos_invalidos),
+            simbolos_invalidos_unicode,
+        )
+        self.assertListEqual(
+            ocultar.convertir_texto_a_binario(simb_sencilla),
+            simb_sencilla_unicode,
+        )
+
+    def test_modifica_lsb(self):
+        self.assertEqual(ocultar.modifica_lsb(15, 0), 14)
+        self.assertEqual(ocultar.modifica_lsb(15, 1), 15)
+        self.assertEqual(ocultar.modifica_lsb(0, 1), 1)
+        self.assertEqual(ocultar.modifica_lsb(0, 0), 0)
+
+    def test_ocultar_car_bin_en_arr(self):
+        arr = [
+            [12, 12, 12],
+            [14, 14, 14],
+            [15, 15, 15],
+            [24, 24, 24],
+            [36, 36, 36],
+            [37, 37, 37],
+            [16, 16, 16],
+        ]
+        ocultar.ocultar_car_bin_en_arr('01100001', arr, 3)
+        arr_esperado = [
+            [12, 12, 12],
+            [14, 14, 14],
+            [15, 15, 15],
+            [24, 24, 25],
+            [37, 36, 36],
+            [36, 36, 37],
+            [16, 16, 16],
+        ]
+        self.assertListEqual(arr, arr_esperado)
+
+    def test_ocultar_binario_en_imagen(self):
+        img = Image.open('imagenes/fac.png')
+        ancho, alto = img.size
+
+        s = ' '
+        s = s.join([str(i) for i in range(ancho*alto + 3)])
+        txt_bin = ocultar.convertir_texto_a_binario(s)
+        self.assertRaises(Exception, ocultar.ocultar_binario_en_imagen, [txt_bin, img])
+
+        txt_bin = ocultar.convertir_texto_a_binario('a')
+        arr_esperado = [[15, 15, 15], [21, 20, 20], [30, 30, 31]]
+        arr_obtenido = (
+            ocultar.ocultar_binario_en_imagen(txt_bin, img)
+            .reshape((ancho * alto, 3))
+            .tolist()[:3]
+        )
+        self.assertListEqual(arr_obtenido, arr_esperado)
+
+        txt_bin = ocultar.convertir_texto_a_binario('Pæpá')
+        arr_esperado = [
+            [15, 15, 15],
+            [21, 21, 20],
+            [30, 30, 30],
+            [32, 32, 32],
+            [19, 18, 18],
+            [12, 12, 12],
+            [16, 16, 17],
+            [13, 13, 12],
+            [16, 16, 16],
+            [19, 19, 19],
+            [23, 22, 22],
+            [26, 26, 27],
+            [26, 26, 27],
+            [22, 23, 23],
+            [19, 18, 18],
+            [15, 15, 15],
+            [19, 19, 19],
+            [22, 23, 22],
+        ]
+        arr_obtenido = (
+            ocultar.ocultar_binario_en_imagen(txt_bin, img)
+            .reshape((ancho * alto, 3))
+            .tolist()[:18]
+        )
+        self.assertListEqual(arr_obtenido, arr_esperado)
+
+if __name__ == '__main__':
+    unittest.main()
